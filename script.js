31531a7284be10a26a64ca98772d7a135f516ba8
unknown <caylenepicache@gmail.com>
2019-03-26 22:12:16 -0700

    finalize constant tutorial

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 window.onload = function (){
     const pi = 3.142;
-    pi = 10;
-    console.log(pi)
+
+    function calcArea (r) {
+        const pi = 10;
+        console.log("the area is: " + pi * r * r);
+
+    }
+
+    console.log(pi);
+    calcArea(5);
 }
