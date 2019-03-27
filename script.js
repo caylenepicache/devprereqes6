659f9c0c850e9a4477f6471376bdeb6ed41d31b7
unknown <caylenepicache@gmail.com>
2019-03-26 22:17:02 -0700

    ul and function onclick{

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 window.onload = function (){
     
 
-    var x = 10;
+    var items  = documents.getElementByTagName("li");
 
-    if(x > 5){
-        let x = 5;
-        console.log("inside: " + x);
+    for(var x = 0; x < items.length; x++){
+        items[x].onclick = function(){
+            console.log(x);
+        }
     }
-
-    console.log("outside: " + x);
 }
 
