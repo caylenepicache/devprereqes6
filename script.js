29fe9b804b341090d35776ddb8e20c6c0a7c4f96
unknown <caylenepicache@gmail.com>
2019-03-28 21:04:52 -0700

    tempalte st rings

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = function (){
     
     function logNinja(name,age){
-        console.log("my name is " + name + "and my age is " + age);
+        console.log('my name is ${name} and my age is ${age}');
 
     }
 
