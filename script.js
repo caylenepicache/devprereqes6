0ca98943de60834b61033d486b448b32e3f79539
unknown <caylenepicache@gmail.com>
2019-03-28 20:59:25 -0700

    spread operator ..nums

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 window.onload = function (){
     
-function logNinja(name="Ryu", belt="Red", age=25) {
-    console.log("my name is " + name + "and my belt color is " + belt + "and my age is " + age);
+    var nums = [3,5,70];
+    
+function addNums(a,b,c) {
+    console.log(a+b+c);
 }
 
-logNinja();
+addNums(..nums);
 }
 
