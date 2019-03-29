094b63dc3de8ca43c7a294736ce3acd1e24ac95a
unknown <caylenepicache@gmail.com>
2019-03-28 21:44:42 -0700

    object literal enhacenments function

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ window.onload = function (){
     var name = "Crystal";
     var belt = "Black";
     var ninja = {
-        name: name,
-        belt: belt
+        name, belt, 
+        chop: function(x){
+            console.log('you chopped the enemy ${x} times')
+        }
     };
 
-    console.log(ninja.name);
+    console.log(ninja.chop(5));
 }
 
