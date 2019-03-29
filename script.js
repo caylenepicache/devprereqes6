8c3bfad595932f197a6d49c9956572c4843237bc
unknown <caylenepicache@gmail.com>
2019-03-28 20:54:29 -0700

    default parameters are directly added in the arguement

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 window.onload = function (){
     
+function logNinja(name="Ryu", belt="Red", age=25) {
+    console.log("my name is " + name + "and my belt color is " + belt + "and my age is " + age);
+}
 
-    var items  = documents.getElementByTagName("li");
-
-    for(var x = 0; x < items.length; x++){
-        items[x].onclick = function(){
-            console.log(x);
-        }
-    }
-
-    console.log(x);
+logNinja();
 }
 
