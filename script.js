1dd50eeadbe1dccf9ef2422ea8ca06d663f084f7
unknown <caylenepicache@gmail.com>
2019-03-28 22:05:35 -0700

    sets

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ window.onload = function (){
 
  names.add("Shaun").add("Ryu").add("Crystal");
  names.delete("Crystal")
+ names.clear();
+ console.log(names.has("Crystal"))
  console.log(names.size);
 
  console.log(names);
