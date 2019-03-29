812bbe6cd9c0e4754fcea9bf60555156e29e98bb
unknown <caylenepicache@gmail.com>
2019-03-28 21:51:57 -0700

    new string methods

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 window.onload = function (){
     
-    var name = "Crystal";
-    var belt = "Black";
-    var ninja = {
-        name, belt, 
-        chop: function(x){
-            console.log('you chopped the enemy ${x} times')
-        }
-    };
-
-    console.log(ninja.chop(5));
+var str = "goodbye";
+console.log(str.startsWith("bye", 4));
 }
 
